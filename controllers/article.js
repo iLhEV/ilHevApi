7306460fbd613b18070ec530e950afd89407e8f9
iadomyshev <iadomyshev@gmail.com>
2022-03-26 18:27:10 +0400

    Fix success logic.

--- controllers/article.js
+++ controllers/article.js
@@ -1,15 +1,13 @@
 import pool from "../db/pool.js";
-import telegram from "../bot/telegram.js";
 
 const createArticle = async (req, res) => {
   const article = req.body;
-  res.send({success: true})
-    pool.query('insert into articles(text) values($1)', [article.text], async (error, results) => {
-        if (error) {
-            throw error
-        }
-        console.log('article is added');
-    })
+  pool.query('insert into articles(text) values($1)', [article.text], async (error, results) => {
+    if (error) {
+        throw error
+    }
+    res.send({ success: true });
+  })
 }
 
 export default { createArticle };

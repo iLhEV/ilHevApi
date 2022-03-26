1537a04d0edd7c5c984d3b5d364a84f88e35940e
iadomyshev <iadomyshev@gmail.com>
2022-03-26 18:44:13 +0400

    Prepare logs for debug.

--- controllers/article.js
+++ controllers/article.js
@@ -1,12 +1,15 @@
 import pool from "../db/pool.js";
 
 const createArticle = async (req, res) => {
+  console.log('run route articles.createArticle')
   const article = req.body;
   pool.query('insert into articles(text) values($1)', [article.text], async (error, results) => {
     if (error) {
         throw error
     }
+    console.log('article added success');
     res.send({ success: true });
+    console.log('article added message success');
   })
 }
 

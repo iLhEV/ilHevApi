442eeaa4459fb11825970a3fabc19099b51cfe06
iadomyshev <iadomyshev@gmail.com>
2022-03-27 17:49:26 +0400

    List articles method.

--- controllers/article.js
+++ controllers/article.js
@@ -1,17 +1,26 @@
 import pool from "../db/pool.js";
 
 const createArticle = async (req, res) => {
-  console.log('run route articles.createArticle')
   const article = req.body;
   pool.query('insert into articles(text) values($1)', [article.text], async (error, results) => {
     if (error) {
+        console.error('error add article');
         throw error
     }
-    console.log('article added success');
+    console.log('article added');
     res.send({ success: true });
-    console.log('article added message success');
-  })
+  });
 }
 
-export default { createArticle };
+const showList = async (request, response) => {
+  pool.query("select * from articles order by id desc", async (error, res) => {
+    if (error) {
+      console.error('error get articles');
+      throw error
+    }
+    console.log('articles sent, count: ', res.rowCount);
+    response.send({ success: true, data: res.rows, meta: { count: res.rowCount } });
+  });
+}
 
+export default { createArticle, showList };
